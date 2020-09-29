b5c496bf6b9c178129a18ef45f5780ebe7d19127
ebussiere <eddy@embtech.ca>
2020-09-29 11:19:32 +0000

    added redirect

--- express_server.js
+++ express_server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080;
+
+//Middleware
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 //Data for excercise
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+const generateRandomString = function(length = 6) {
+  return Math.random().toString(20).substr(2, length);
 };
 //"Home" route
 app.get('/', function(req, res) {
@@ -17,10 +26,31 @@ app.get('/urls', function(req, res) {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', function(req, res) {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_new', templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const longurl = urlDatabase[req.params.shortURL];
   const templateVars = { shortURL: req.params.shortURL, longURL: longurl };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body["longURL"];  // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`);
+  console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/getdata", function(req, res) {
+  pullData().then(function(data) {
+    return filterByYear(data);
+  }).then(function(filteredData) {
+    res.json(filteredData);
+  });
 });
 //Server running at port
 app.listen(PORT, () => {
