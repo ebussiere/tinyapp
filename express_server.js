095acdb25708c59e1eadccee3adb854db35d32db
ebussiere <eddy@embtech.ca>
2020-09-29 12:21:06 +0000

    added edit url function

--- express_server.js
+++ express_server.js
@@ -38,20 +38,38 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body["longURL"];  // Log the POST request body to the console
   res.redirect(`/urls/${shortURL}`);
   console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/getdata", function(req, res) {
-  pullData().then(function(data) {
-    return filterByYear(data);
-  }).then(function(filteredData) {
-    res.json(filteredData);
-  });
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+  console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
 });
+
+app.get("/urls/show/:shortURL", (req, res) => {
+  //urlDatabase[shortURL] = req.params.longURL;
+  res.redirect(`/urls/show/${req.params.shortURL}`);
+  //console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls`);
+  console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
+});
+
+// app.get("/getdata", function(req, res) {
+//   pullData().then(function(data) {
+//     return filterByYear(data);
+//   }).then(function(filteredData) {
+//     res.json(filteredData);
+//   });
+// });
 //Server running at port
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}.`);
