dc1a666ba504be8e838b0c4d2f5905fc69b2e28b
ebussiere <eddy@embtech.ca>
2020-09-29 16:01:43 +0000

    prior to merge

--- express_server.js
+++ express_server.js
@@ -26,24 +26,37 @@ app.get('/', function(req, res) {
 });
 // about page
 app.get('/urls', function(req, res) {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', function(req, res) {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_new', templateVars);
 });
 
 app.get("/urls/show/:shortURL", (req, res) => {
   //urlDatabase[shortURL] = req.params.longURL;
-  res.redirect(`/urls/show/${req.params.shortURL}`);
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.redirect(`/urls/show/${req.params.shortURL}`, templateVars);
   //console.log(urlDatabase);         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const longurl = urlDatabase[req.params.shortURL];
-  const templateVars = { shortURL: req.params.shortURL, longURL: longurl };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: longurl,
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -69,6 +82,12 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   console.log(req.params.username);
+  res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  //console.log(req.params.username);
   res.redirect(`/urls`);
 });
 
