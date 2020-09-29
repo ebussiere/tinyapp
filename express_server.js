85161a8d671b8f37e23cb8727cacacc29aa5a73f
ebussiere <eddy@embtech.ca>
2020-09-29 01:30:22 +0000

    index_urls.ejs created

--- express_server.js
+++ express_server.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.set('view engine', 'ejs');
 //Data for excercise
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 //"Home" route
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get('/', function(req, res) {
+  res.render('pages/index');
 });
-
-//"Home" route
-
-//"Home" route
-app.get("/urls.json", (req, res) => {
-  res.send(urlDatabase);
+// about page
+app.get('/urls', function(req, res) {
+  const templateVars = { urls: urlDatabase };
+  res.render('pages/urls_index', templateVars);
 });
-
 //Server running at port
 app.listen(PORT, () => {
   console.log(`Listening on Port: ${PORT}.`);
